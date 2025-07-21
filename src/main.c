188cec6f8a7ccaac0930cfcdf455389343148be4
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-21 07:16:08 +0530

    added func definitions

--- src/main.c
+++ src/main.c
@@ -3,6 +3,19 @@
 #include "mmu.h"
 #include "rom.h"
 
+/**
+ * main:
+ * Entry point of the emulator.
+ * 
+ * Arguments: 
+ * @argc: Number of command-line arguments.
+ * @argv: Array of argument strings.
+ *
+ * Loads the ROM and starts the emulation loop.
+ *
+ * Return: 
+ * 0 on success, non-zero on failure.
+ */
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: %s <ROM file>\n", argv[0]);
@@ -10,13 +23,23 @@ int main(int argc, char *argv[]) {
     }
 
     init_mmu();
-    load_rom(argv[1]);
+
+    if (!load_rom(argv[1])) {
+        printf("Failed to load ROM.\n");
+        return 1;
+    }
 
     cpu_reset();
 
     while (1) {
+        // Execute one instruction per cycle
         cpu_step();
-        // TODO: Handle timers, interrupts, graphics, etc.
+
+        // Future work:
+        // - Handle timers
+        // - Render graphics
+        // - Handle input
+        // - Trigger interrupts
     }
 
     return 0;
