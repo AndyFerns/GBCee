812fb83e00f4c605574d1616241a32d39291c6ca
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-21 07:49:55 +0530

    edited function declarations

--- includes/mmu.h
+++ includes/mmu.h
@@ -23,9 +23,14 @@ void init_mmu();
 uint8_t mmu_read(uint16_t addr);
 
 /**
- * mmu_write - Writes a byte to the specified address.
+ * mmu_write - 
+ * Writes a byte to the specified address.
+ * 
+ * Parameters: -
  * @addr: 16-bit memory address.
  * @value: Byte to write.
+ * 
+ * Return: void
  */
 void mmu_write(uint16_t addr, uint8_t value);
 
