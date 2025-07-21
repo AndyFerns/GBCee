4a4a9a270e8a794c1b050b35fd0fb7300f050a83
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-21 07:50:24 +0530

    initialized MMU cartridge ROM region + 64KKB memory space (simplified)

---
+++ src/mmu.c
@@ -0,0 +1,29 @@
+#include "mmu.h"
+#include <string.h>
+
+static uint8_t rom[0x8000]; // Cartridge ROM region
+static uint8_t ram[0x10000]; // Full 64KB memory space (simplified)
+
+/**
+ * init_mmu - See header.
+ */
+void init_mmu() {
+    memset(ram, 0, sizeof(ram));
+}
+
+/**
+ * mmu_read - See header.
+ */
+uint8_t mmu_read(uint16_t addr) {
+    if (addr < 0x8000)
+        return rom[addr]; // ROM is read-only
+    return ram[addr];
+}
+
+/**
+ * mmu_write - See header.
+ */
+void mmu_write(uint16_t addr, uint8_t value) {
+    if (addr < 0x8000) return; // ROM is read-only
+    ram[addr] = value;
+}
