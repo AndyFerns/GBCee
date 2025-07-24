e83193830ee194f426438d5d815d1ded6c47a2a6
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-24 13:23:25 +0530

    added alu suite func declarations

---
+++ includes/alu.h
@@ -0,0 +1,15 @@
+#ifndef ALU_H
+#define ALU_H
+
+#include <stdint.h>
+
+// Arithmeticv operations 
+void ADD_A(uint8_t val);
+void SUB_A(uint8_t val);
+void AND_A(uint8_t val);
+void OR_A(uint8_t val);
+void XOR_A(uint8_t val);
+void CP_A(uint8_t val);
+
+
+#endif
