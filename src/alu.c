cbf33d2aaab32240f8a11c707413cb182dfe066d
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-24 14:34:08 +0530

    added ADD_A func definition

---
+++ src/alu.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "cpu.h"
+
+/* Flag bit Masks 
+ * GameBoy CPU:
+ * 
+ * Zero (Z)
+ * Subtract (N)
+ * Half-Carry (H)
+ * Carry (C)
+ */
+#define FLAG_Z 0x80
+#define FLAG_N 0x40
+#define FLAG_H 0x20
+#define FLAG_C 0x10
+
+// Addition operator ADD
+/**
+ * ADD_A - Adds val to register A.
+ * 
+ * Parameters:
+ *  @val: 8-bit value to add to A
+ */
+void ADD_A(uint8_t val) {
+    uint16_t result = cpu.A + val;
+    cpu.F = 0;
+    if ((result & 0xFF) == 0) {
+        cpu.F |= FLAG_Z;
+    }
+    if ((cpu.A & 0x0F) + (val & 0x0F) > 0x0F) {
+        cpu.F |= FLAG_H;
+    }
+    if (result > 0xFF) {
+        cpu.F |= FLAG_C;
+    }
+    cpu.A = result & 0xFF;
+}
+
+// SUB r
+void SUB_A_R(R) { 
+    do { 
+        uint8_t val = cpu.R; 
+        cpu.F = FLAG_N; 
+        if ((cpu.A & 0x0F) < (val & 0x0F)) cpu.F |= FLAG_H; 
+        if (cpu.A < val) cpu.F |= FLAG_C; 
+        cpu.A -= val; 
+        if (cpu.A == 0) cpu.F |= FLAG_Z; 
+    } while (0);
+}
+
+// AND r
+void SUB_A_R(R) { 
+    do {
+        uint8_t val = cpu.R; 
+        cpu.F = FLAG_N; 
+        if ((cpu.A & 0x0F) < (val & 0x0F)) cpu.F |= FLAG_H; 
+        if (cpu.A < val) cpu.F |= FLAG_C; 
+        cpu.A -= val; 
+        if (cpu.A == 0) cpu.F |= FLAG_Z; 
+    } while (0);
+}
+
+// OR r, XOR r, CP r yet to be implemented
