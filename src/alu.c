addcf5efcad2227da8124d510a3ba109eca1c98b
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-25 17:10:44 +0530

    added some misc alu operators

--- src/alu.c
+++ src/alu.c
@@ -16,6 +16,15 @@
 #define FLAG_H 0x20
 #define FLAG_C 0x10
 
+/* ARITHMETIC OPERATIONS */
+/** 
+ * ADD_A
+ * ADC_A
+ * SUB_A
+ * SBC_A
+ * CP_A
+ */
+
 // Addition operator ADD
 /**
  * ADD_A - Adds val to register A.
@@ -39,7 +48,35 @@ void ADD_A(uint8_t val) {
     cpu.A = result & 0xFF;
 }
 
-// SUB r
+
+/**
+ * @brief Add val + carry flag to A
+ * 
+ * @param val:uint8_t 8-bit value to add to A
+ * 
+ * @return void
+ */
+void ADC_A(uint8_t val) {
+    uint8_t carry = (cpu.F & FLAG_C) ? 1 : 0;
+    uint16_t result = cpu.A + val + carry;
+
+    cpu.F = 0; // reset flag
+
+    if ((result & 0xFF) == 0) {
+        cpu.F |= FLAG_Z;
+    }
+    if ((cpu.A & 0x0F) + (val & 0x0F) + carry > 0x0F) {
+        cpu.F |= FLAG_H;
+    }
+    if (result > 0xFF) {
+        cpu.F |= FLAG_C;
+    }
+
+    cpu.A = result & 0xFF;
+}
+
+
+// SUB n
 /**
  * @brief SUB_A - Subtracts value from register A
  *
@@ -61,6 +98,64 @@ void SUB_A(uint8_t val) {
         cpu.F |= FLAG_Z;
     }
 }
+
+
+/**
+ * @brief Sub val + carry flag to A
+ * 
+ * @param val:uint8_t 8-bit value to add to A
+ * 
+ * @return void
+ */
+void SBC_A(uint8_t val) {
+    uint8_t carry = (cpu.F & FLAG_C) ? 1 : 0;
+    uint16_t result = cpu.A - val - carry;
+
+    cpu.F = FLAG_N;
+
+    if ((result & 0xFF) == 0) {
+        cpu.F |= FLAG_Z;
+    }
+    if ((cpu.A & 0xFF) < (val & 0x0F) + carry) {
+        cpu.F |= FLAG_H;
+    }
+    if (cpu.A < (val + carry)) {
+        cpu.F |= FLAG_C;
+    }
+}
+
+/**
+ * simple comparison between A and val.
+ * 
+ * result is not stored.
+ * 
+ * @param val 8-bit integer to compare A with
+ * 
+ * @return void
+ */
+void CP_A(uint8_t val) {
+    cpu.F = FLAG_N;
+    if ((cpu.A & 0x0F) < (val & 0x0F)) {
+        cpu.F |= FLAG_H;
+    }
+    if (cpu.A < val) {
+        cpu.F |= FLAG_C;
+    }
+    uint8_t result = cpu.A - val;
+    if (result == 0) {
+        cpu.F |= FLAG_Z;
+    }
+}
+
+
+
+/* LOGICAL OPERATIONS */
+/**
+ * AND r
+ * OR r
+ * XOR r
+ */
+
 
 // AND r
 /**
@@ -109,25 +204,77 @@ void XOR_A(uint8_t val) {
     }
 }
 
+
+/* MISCELLANEOUS OPERATORS */
+
 /**
- * simple comparison between A and val.
+ * SWAP n
+ * DAA()
+ * CPL()
+ */
+
+/**
+ *  1. SWAP n -TBD
+ */
+
+/**
+ * @brief DAA
+ * decimal adjust register A
+ * this instr adjusts reg A so that the correct representation of 
+ * Binary Coded Decimal (BCD) is obtained
  * 
- * result is not stored.
+ * uses opcode 0x27
  * 
- * @param val 8-bit integer to compare A with
+ * @param none
+ * 
+ * @return none
+ */
+void DAA() {
+    uint8_t a = cpu.A;
+    uint8_t adjust = 0;
+    uint8_t carry = 0;
+
+    // after ADD
+    if (!(cpu.F & FLAG_N)) {
+        if ((cpu.F & FLAG_H) || (a & 0xFF) > 9) {
+            adjust |= 0x06;
+        }
+
+        if ((cpu.F & FLAG_C) || a > 0x99) {
+            adjust |= 0x60;
+            carry = 1;
+        }
+        a += adjust;
+    } else {
+        // after SUB
+        if (cpu.F & FLAG_H) {
+            adjust |= 0x06;
+        }
+        if (cpu.F & FLAG_C) {
+            adjust |= 0x60;
+        }
+        a -= adjust;
+    }
+}
+
+
+/**
+ * @brief complement A register (flips all bits)
+ * 
+ * Flags affected:
+ * Z - Not affected.
+ * N - Set.
+ * H - Set.
+ * C - Not affected.
+ * 
+ * uses opcode 0x2F
+ * 
+ * @param none
  * 
  * @return void
+ * 
  */
-void CP_A(uint8_t val) {
-    cpu.F = FLAG_N;
-    if ((cpu.A & 0x0F) < (val & 0x0F)) {
-        cpu.F |= FLAG_H;
-    }
-    if (cpu.A < val) {
-        cpu.F |= FLAG_C;
-    }
-    uint8_t result = cpu.A - val;
-    if (result == 0) {
-        cpu.F |= FLAG_Z;
-    }
+void CPL() {
+    cpu.A = ~cpu.A;
+    cpu.F |= FLAG_N | FLAG_H;
 }
