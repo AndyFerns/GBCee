ea8c77d55b12e67f665119b397f169f9f10f6f9f
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-25 15:43:35 +0530

    added new arithmetic op func declarations

--- includes/alu.h
+++ includes/alu.h
@@ -6,11 +6,22 @@
 // Arithmeticv operations 
 
 void ADD_A(uint8_t val);
+void ADC_A(uint8_t val);
 void SUB_A(uint8_t val);
+void SBC_A(uint8_t val);
+void CP_A(uint8_t val);
+
+// Logical Operations
+
 void AND_A(uint8_t val);
 void OR_A(uint8_t val);
 void XOR_A(uint8_t val);
-void CP_A(uint8_t val);
 
+// misc ALU ops
+
+uint8_t INC(uint8_t val);
+uint8_t DEC(uint8_t val);
+void DAA();
+void CPL();
 
 #endif
