20496b6ea30f58c7106498e44a557bf90f31afab
AndyFerns <write2andrew.fernandes@gmail.com>
2025-08-24 04:17:00 +0530

    changed enum structure + load_rom more verbose

--- includes/rom.h
+++ includes/rom.h
@@ -10,24 +10,28 @@
 * 0x0150+	            Start of game code          (entry point)
 */
 
-typedef enum mbc_t {
-    MBC_NONE,
-    MBC1,
-    MBC3,
-    MBC5,
-    MBC_UNKNOWN,
-} mbc_t;
+/** @brief Enum for MBC allocation */
+typedef enum mbc_type_t {
+    MBC_TYPE_NONE,
+    MBC_TYPE_MBC1,
+    MBC_TYPE_MBC3,
+    MBC_TYPE_MBC5,
+    MBC_TYPE_UNKNOWN,
+} mbc_type_t;
 
-extern mbc_t mbc_type;
+extern mbc_type_t mbc_type;
 
 /**
- * load_rom - Loads a Game Boy ROM into memory.
+ * @brief load_rom - loads a ROM file, allocates memory for it, and parses its header
  * 
  * Parameters: 
- * @path: Path to the ROM file.
+ * @param path Path to the ROM file.
+ * @param out_rom_data pointer to an uint8_t* that will be set to the allocated rom data
+ * @param out_rom_size pointer to a size_t that will be set to the size of the rom
+ * @param out_mbc_type pointer to an mbc_type_t
  *
- * Return: 1 on success, 0 on failure.
+ * @returns 1 on success, 0 on failure.
  */
-int load_rom(const char* path);
+int load_rom(const char* path, uint8_t* out_rom_data, size_t* out_rom_size, mbc_type_t out_mbc_type;
 
 #endif
