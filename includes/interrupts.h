0dce69af0d8b3b8c1a95e9da9ba9d743c5a81282
AndyFerns <write2andrew.fernandes@gmail.com>
2025-09-07 01:54:44 +0530

    removed static service interrupts function from headerfile

--- includes/interrupts.h
+++ includes/interrupts.h
@@ -4,25 +4,6 @@
 #include "alu.h" // for push16
 
 /**
- * @brief function to service a specific interrupt
- * 
- * disables global interrupts when an interrupt is serviced 
- * 
- * corresponding bit in the IF register is cleared
- * 
- * current PC is pushed onto the stack
- * 
- * cpu jumps to the specific interrupt vector address
- * 
- * @param interrupt_bit the corresponding interrupt bit  
- * 
- * @note static defined 
- * 
- * @returns void 
- */
-static void service_interrupts(int interrupt_bit);
-
-/**
  * @brief Handles the CPU interrupt cycle
  * 
  * @details This function should be called once per cpu cycle 
