a88dd33dbdcc6fef692baeed7ff05b11765b7bdb
AndyFerns <write2andrew.fernandes@gmail.com>
2025-09-07 01:38:20 +0530

    added service_interrupts method to headerfile

--- includes/interrupts.h
+++ includes/interrupts.h
@@ -4,6 +4,25 @@
 #include "alu.h" // for push16
 
 /**
+ * @brief function to service a specific interrupt
+ * 
+ * disables global interrupts when an interrupt is serviced 
+ * 
+ * corresponding bit in the IF register is cleared
+ * 
+ * current PC is pushed onto the stack
+ * 
+ * cpu jumps to the specific interrupt vector address
+ * 
+ * @param interrupt_bit the corresponding interrupt bit  
+ * 
+ * @note static defined 
+ * 
+ * @returns void 
+ */
+static void service_interrupts(int interrupt_bit);
+
+/**
  * @brief Handles the CPU interrupt cycle
  * 
  * @details This function should be called once per cpu cycle 
@@ -17,5 +36,6 @@
  * @returns void
  */ 
 void handle_interrupts();
+
 
 #endif
